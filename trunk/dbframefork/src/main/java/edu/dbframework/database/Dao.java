eac4defb5a43810831de90d7fc1b3d21c6343a72
GreenPoser@gmail.com <GreenPoser@gmail.com@7f237a6e-47cb-8321-6dcf-f729e088ee01>
2013-10-27 13:30:06 +0000

    deleted unused classes

--- trunk/dbframefork/src/main/java/edu/dbframework/database/Dao.java
+++ trunk/dbframefork/src/main/java/edu/dbframework/database/Dao.java
@@ -61,37 +61,6 @@ public class Dao {
 
         return new JdbcTemplate(dataSource).execute(psc, pscb);
     }
-    /*public Map<String, List<String>> getData(String query) {
-        ResultSet rs = null;
-        Statement stmt = null;
-        Connection con = null;
-        Map<String, List<String>> data = null;
-
-        try {
-            con = dataSource.getConnection();
-            stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            rs = stmt.executeQuery(query.toString());
-            data = new LinkedHashMap<String, List<String>>();
-
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnsCount = rsmd.getColumnCount();
-
-            for (int i = 1; i <= columnsCount; i++) {
-                ArrayList<String> colData = new ArrayList<String>();
-                if (rs.first()) {
-                    while (rs.next()) {
-                        colData.add(rs.getString(i));
-                    }
-                    data.put(rsmd.getColumnLabel(i), colData);
-                    rs.beforeFirst();
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(
-                    "Exception in Dao.getData(tableItem)", e);
-        }
-       return data;
-    }*/
 
     public DataSource getDataSource() {
         return dataSource;
