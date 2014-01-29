f53b65654f3a4e64a9c0d02191090d55367928ef
GreenPoser@gmail.com <GreenPoser@gmail.com@7f237a6e-47cb-8321-6dcf-f729e088ee01>
2014-01-29 20:09:36 +0000

    added file-menu, connection checking, etc

---
+++ gui-swing/src/main/java/gui/table/DataTableModel.java
@@ -0,0 +1,79 @@
+package gui.table;
+
+import edu.dbframework.parse.beans.items.ColumnItem;
+import edu.dbframework.parse.beans.items.TableItem;
+import edu.dbframework.service.Service;
+import gui.Main;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class DataTableModel extends AbstractTableModel {
+	
+	private Map<String, List<String>> data;
+	private List<String> columnNames;
+
+    private Service service = (Service) Main.context.getBean("service");
+
+    private String tableName;
+	
+	public DataTableModel(TableItem item) {
+        this.tableName = item.getName();
+        this.data = service.getDataForTableItem(item);
+		this.columnNames = new ArrayList<String>();
+        prepareColumnNames(item);
+    }
+
+    public DataTableModel(TableItem tableItem, List<String> links, ColumnItem column, String referTable) {
+        this.tableName = tableItem.getName();
+        this.data = service.getDataByRows(tableItem, links, column, referTable);
+        this.columnNames = new ArrayList<String>();
+        prepareColumnNames(tableItem);
+    }
+
+    public DataTableModel(TableItem tableItem, String primaryKey, String indexColumn) {
+        this.tableName = tableItem.getName();
+        this.data = service.getDataByRelationColumn(tableItem, primaryKey, indexColumn);
+        this.columnNames = new ArrayList<String>();
+        prepareColumnNames(tableItem);
+    }
+
+    private void prepareColumnNames(TableItem item) {
+        for (ColumnItem columnItem : item.getColumns()) {
+            if (columnItem.getAlias() != null) {
+                if (!columnItem.getAlias().equals(""))
+                    this.columnNames.add(columnItem.getAlias());
+            } else {
+                this.columnNames.add(columnItem.getName());
+            }
+        }
+    }
+
+	@Override
+	public int getRowCount() {
+		return data.get(columnNames.get(0)).size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return this.columnNames.size();
+	}
+	
+	@Override
+	public String getColumnName(int col) {
+        return columnNames.get(col);
+    }
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		String column = this.columnNames.get(columnIndex);
+		return data.get(column).get(rowIndex);
+	}
+
+    public String getTableName() {
+        return tableName;
+    }
+}
---
+++ gui-swing/src/main/java/gui/table/util/PropertyUtility.java
@@ -0,0 +1,65 @@
+package gui.table.util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GreenPoser
+ * Date: 29.01.14
+ * Time: 21:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class PropertyUtility {
+
+    private final static String propertyPath = "src/main/resources/jdbc.properties";
+
+    public final static String userProp = "jdbc.user";
+    public final static String passwordProp = "jdbc.password";
+    public final static String driverProp = "jdbc.driverClassName";
+    public final static String urlProp = "jdbc.url";
+
+    private Properties properties = new Properties();
+
+    private static PropertyUtility instance;
+
+    private PropertyUtility() {
+        InputStream input = null;
+        try {
+            input = new FileInputStream(propertyPath);
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static PropertyUtility getInstance() {
+        if (instance == null) {
+            instance = new PropertyUtility();
+        }
+        return instance;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public void setProperty(String key, String value) {
+        OutputStream output = null;
+        try {
+            output = new FileOutputStream(propertyPath);
+            properties.setProperty(key, value);
+            properties.store(output, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
