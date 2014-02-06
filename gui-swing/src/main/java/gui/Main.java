efd7c05408d71c0c9c6157f63ecc5762610630ad
GreenPoser@gmail.com <GreenPoser@gmail.com@7f237a6e-47cb-8321-6dcf-f729e088ee01>
2014-02-06 06:42:14 +0000

    added frame package

--- gui-swing/src/main/java/gui/Main.java
+++ gui-swing/src/main/java/gui/Main.java
@@ -1,23 +1,11 @@
 package gui;
 
-import edu.dbframework.database.MetadataDao;
-import edu.dbframework.parse.beans.DatabaseBean;
-import edu.dbframework.parse.beans.items.TableItem;
-import edu.dbframework.parse.helpers.DatabaseManager;
-import gui.table.DataTable;
-import gui.table.DataTableModel;
+import gui.frame.MainFrame;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 
 public class Main {
 
@@ -33,7 +21,7 @@ public class Main {
 				try {
                     UIManager.setLookAndFeel(
                             UIManager.getSystemLookAndFeelClassName());
-                    MainFrame window = new MainFrame();
+                    MainFrame window = (MainFrame) context.getBean("mainFrame");
                     window.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -41,4 +29,8 @@ public class Main {
 			}
 		});
 	}
+
+    public static Object getBeanFromContext(String name) {
+        return context.getBean(name);
+    }
 }
