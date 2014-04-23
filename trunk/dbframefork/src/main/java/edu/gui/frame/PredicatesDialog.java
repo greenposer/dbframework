9f48e7f03b6ca177f9a4e38fac7fc61247ddbdea
GreenPoser@gmail.com <GreenPoser@gmail.com@7f237a6e-47cb-8321-6dcf-f729e088ee01>
2014-04-23 15:05:26 +0000

    some chngs, start working with predicates

---
+++ trunk/dbframefork/src/main/java/edu/gui/frame/PredicatesDialog.java
@@ -0,0 +1,13 @@
+package edu.gui.frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PredicatesDialog extends JDialog {
+
+    public PredicatesDialog() {
+        setTitle("Create New Connection");
+        setBounds(100, 100, 350, 250);
+        getContentPane().setLayout(new GridBagLayout());
+    }
+}
