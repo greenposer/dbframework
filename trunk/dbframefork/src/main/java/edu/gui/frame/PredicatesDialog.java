1a6dd42a332b5348dfec2a13ae596ebcf1166845
GreenPoser@gmail.com <GreenPoser@gmail.com@7f237a6e-47cb-8321-6dcf-f729e088ee01>
2014-04-28 06:05:37 +0000

    PredicatesDialog buttons

--- trunk/dbframefork/src/main/java/edu/gui/frame/PredicatesDialog.java
+++ trunk/dbframefork/src/main/java/edu/gui/frame/PredicatesDialog.java
@@ -6,6 +6,8 @@ import edu.gui.table.DataTableModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.*;
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class PredicatesDialog extends JDialog {
     static ArrayList<JTextField> predicates;
 
     public PredicatesDialog() {
-        setTitle("Create New Connection");
+        setTitle("Add predicates");
         setBounds(150, 150, 450, 350);
         init();
     }
@@ -25,20 +27,33 @@ public class PredicatesDialog extends JDialog {
     private void init() {
         panel = new JPanel(new GridBagLayout());
         predicates = new ArrayList<JTextField>(MainFrame.table.getColumnCount());
-        JLabel label;
-        JTextField field;
         DataTableModel model = (DataTableModel) MainFrame.table.getModel();
         List<ColumnItem> columns = model.getTableItem().getColumns();
         for (int i = 0; i < columns.size(); i++) {
-            label = new JLabel(columns.get(i).getName());
-            field = new JTextField();
-
-            panel.add(label, new GridBagConstraints(0, i, 1, 1, 0.2, 0, GridBagConstraints.EAST,
+            JLabel label = new JLabel(columns.get(i).getName());
+            JTextField field = new JTextField();
+            panel.add(label, new GridBagConstraints(0, i + 1, 1, 1, 0.3, 0, GridBagConstraints.EAST,
                     GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-            panel.add(field, new GridBagConstraints(1, i, 1, 1, 0.5, 0, GridBagConstraints.WEST,
+            panel.add(field, new GridBagConstraints(1, i + 1, 1, 1, 0.5, 0, GridBagConstraints.WEST,
                     GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
         }
+
+        JButton applyButton = new JButton("Apply");
+        panel.add(applyButton, new GridBagConstraints(0, GridBagConstraints.RELATIVE, 1, 1, 0.5, 0, GridBagConstraints.EAST,
+                GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                PredicatesDialog.this.setVisible(false);
+            }
+        });
+        panel.add(cancelButton, new GridBagConstraints(1, GridBagConstraints.RELATIVE, 1, 1, 0.5, 0, GridBagConstraints.WEST,
+                GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+
         JScrollPane scrollPane = new JScrollPane(panel);
+        this.setPreferredSize(scrollPane.getPreferredSize());
         this.add(scrollPane);
     }
 }
